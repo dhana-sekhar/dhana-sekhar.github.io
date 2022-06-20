67e5614ee7d6f6f90cdb2fa02fee43aae1054671
DhanaSekhar Buddha <buddha.dhanasekhar@gmail.com>
2022-06-20 21:45:45 +0530

    updated

---
+++ precache-manifest.a0fd568b24992aab3d56d1334e54025c.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe518f5aaed224dd05ac9c9b80495fea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc6b1202e6fc2afbbfee",
+    "url": "/static/css/3.cffc9e54.chunk.css"
+  },
+  {
+    "revision": "14f15d1610fe61ed3836",
+    "url": "/static/css/4.54df59d4.chunk.css"
+  },
+  {
+    "revision": "ebe3a56b0767d49a3616",
+    "url": "/static/css/main.e9c8c170.chunk.css"
+  },
+  {
+    "revision": "0186542d911302b29763",
+    "url": "/static/js/2.8b5dcc9f.chunk.js"
+  },
+  {
+    "revision": "dc6b1202e6fc2afbbfee",
+    "url": "/static/js/3.b0038f27.chunk.js"
+  },
+  {
+    "revision": "14f15d1610fe61ed3836",
+    "url": "/static/js/4.07e1d0f3.chunk.js"
+  },
+  {
+    "revision": "ebe3a56b0767d49a3616",
+    "url": "/static/js/main.c20f6164.chunk.js"
+  },
+  {
+    "revision": "945a86ac2a7c2c3c98da",
+    "url": "/static/js/runtime-main.af33d4b9.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "63d18c6dd0ab692af9c28ec562e767fa",
+    "url": "/static/media/Maveric.63d18c6d.jpg"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "0a4e59987c51f57ad663c80c4a3459ab",
+    "url": "/static/media/aws_logo.0a4e5998.svg"
+  },
+  {
+    "revision": "823b4c34d73d341c63354d41a1784585",
+    "url": "/static/media/bcgt_logo.823b4c34.png"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "f46d88805a0d157e5516270d667001c4",
+    "url": "/static/media/ibm_logo.f46d8880.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "19ab3f0afea07255ea960e2d8307774e",
+    "url": "/static/media/machine_learning.19ab3f0a.jpg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "8ce4bfe1a1d5084420224db0889dc7d9",
+    "url": "/static/media/miracle.8ce4bfe1.jpeg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9c3c548a78e47357116f5a08db2bd1fb.js"
+  "/precache-manifest.a0fd568b24992aab3d56d1334e54025c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
